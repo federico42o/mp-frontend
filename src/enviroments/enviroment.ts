89fbc7a2264c353becdb986e55e317c3793b88d7
federicos1 <fedes7777@gmail.com>
2023-02-14 12:04:02 -0300

    ready v1.1

---
+++ src/enviroments/enviroment.ts
@@ -0,0 +1,6 @@
+export const env = {
+    production: false,
+    BACKEND_URL: "http://localhost:8080/test/v1/create",
+    NOTIFICATION_URL : "http://localhost:8080/notification",
+    BACK_URLS: ['htpp://localhost:4200/checkout/success', 'htpp://localhost:4200/checkout/pending', 'htpp://localhost:4200/checkout/failure']
+  };
---
+++ src/enviroments/enviroments.prod.ts
@@ -0,0 +1,6 @@
+  export const env = {
+    production: true,
+    BACKEND_URL: "https://mp-backend-production.up.railway.app/test/v1/create",
+    NOTIFICATION_URL : "https://mp-backend-production.up.railway.app/notification",
+    BACK_URLS: ['http://localhost/checkout/success', 'http://localhost/checkout/pending', 'http://localhost/checkout/failure']
+  };
